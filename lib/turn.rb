5494a7a215dd1ce30ad343ce6e46825265a7274f
gitluky <jluk87@gmail.com>
2017-10-07 03:35:18 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -26,7 +26,7 @@ def position_taken?(board,index)
   end
 end
 
-def move(board, index, symbol = "X")
+def move(board, index, letter = "X")
   if valid_move?(board,index) == true
     board[index] = "#{symbol}"
   end
